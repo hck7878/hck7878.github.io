156e80b694957312e62e4e3f5374cd69e784daea
hck7878 <hck_777@qq.com>
2025-03-23 15:00:44 +0800

    Site updated: 2025-03-23 15:00:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/23/hello-world/"];function toRandomPost(){
+var posts=["2025/03/23/hello-world/","2025/03/23/lmx/","2025/03/23/网站地址/","2025/03/23/bilibili/","2025/03/23/网站说明/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
